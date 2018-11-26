694e5911141f9b5c437309a4b15c5901e8e1a814
BarboraCelesova <barbora.celesova@gmail.com>
2018-11-27 00:34:38 +0100

    Migrated to OF v 1.0

--- Specter.py
+++ Specter.py
@@ -17,7 +17,8 @@ from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
-from ryu.ofproto import ofproto_v1_5
+from ryu.ofproto import ofproto_v1_5, ofproto_v1_0
+from ryu.lib.mac import haddr_to_bin
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
@@ -49,7 +50,8 @@ sem_priority_buffer = threading.Semaphore()
 # TODO Add option for administrator to set some values (ex. number of queues, total buffers length and so on..)
 
 class SimpleSwitch15(app_manager.RyuApp):
-    OFP_VERSIONS = [ofproto_v1_5.OFP_VERSION]
+    #OFP_VERSIONS = [ofproto_v1_5.OFP_VERSION]
+    OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
         global number_of_queues
@@ -69,33 +71,50 @@ class SimpleSwitch15(app_manager.RyuApp):
         except:
             print "Error: unable to start thread"
 
-    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
-    def switch_features_handler(self, ev):
-        datapath = ev.msg.datapath
+    # v 1.5
+    # @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    # def switch_features_handler(self, ev):
+    #     datapath = ev.msg.datapath
+    #     ofproto = datapath.ofproto
+    #     parser = datapath.ofproto_parser
+    #
+    #     # install table-miss flow entry
+    #     #
+    #     # We specify NO BUFFER to max_len of the output action due to
+    #     # OVS bug. At this moment, if we specify a lesser number, e.g.,
+    #     # 128, OVS will send Packet-In with invalid buffer_id and
+    #     # truncated packet data. In that case, we cannot output packets
+    #     # correctly.  The bug has been fixed in OVS v2.1.0.
+    #     match = parser.OFPMatch()
+    #     actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+    #                                       ofproto.OFPCML_NO_BUFFER)]
+    #     self.add_flow(datapath, 0, match, actions)
+
+    # v 1.5
+    # def add_flow(self, datapath, priority, match, actions):
+    #     ofproto = datapath.ofproto
+    #     parser = datapath.ofproto_parser
+    #
+    #     inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+    #                                          actions)]
+    #
+    #     mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+    #                             match=match, instructions=inst)
+    #     datapath.send_msg(mod)
+
+    #v 1.0
+    def add_flow(self, datapath, in_port, dst, src, actions):
         ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
 
-        # install table-miss flow entry
-        #
-        # We specify NO BUFFER to max_len of the output action due to
-        # OVS bug. At this moment, if we specify a lesser number, e.g.,
-        # 128, OVS will send Packet-In with invalid buffer_id and
-        # truncated packet data. In that case, we cannot output packets
-        # correctly.  The bug has been fixed in OVS v2.1.0.
-        match = parser.OFPMatch()
-        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
-                                          ofproto.OFPCML_NO_BUFFER)]
-        self.add_flow(datapath, 0, match, actions)
+        match = datapath.ofproto_parser.OFPMatch(
+            in_port=in_port,
+            dl_dst=haddr_to_bin(dst), dl_src=haddr_to_bin(src))
 
-    def add_flow(self, datapath, priority, match, actions):
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
-
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
-                                             actions)]
-
-        mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
-                                match=match, instructions=inst)
+        mod = datapath.ofproto_parser.OFPFlowMod(
+            datapath=datapath, match=match, cookie=0,
+            command=ofproto.OFPFC_ADD, idle_timeout=0, hard_timeout=0,
+            priority=ofproto.OFP_DEFAULT_PRIORITY,
+            flags=ofproto.OFPFF_SEND_FLOW_REM, actions=actions)
         datapath.send_msg(mod)
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
@@ -122,12 +141,16 @@ class SimpleSwitch15(app_manager.RyuApp):
 
         dst = eth.dst
         src = eth.src
-        in_port = msg.match['in_port']
+        # v 1.5
+        #in_port = msg.match['in_port']
 
 
-        print " Prisiel packetin "
-        self.logger.info("packet in %s ... %s %s %s %s", src_ip, msg.datapath.id, src, dst, in_port)
+        #print " Prisiel packetin "
+        # v 1.5
+        #self.logger.info("packet in %s ... %s %s %s %s", src_ip, msg.datapath.id, src, dst, in_port)
 
+        # v 1.0
+        self.logger.info("packet in %s ... %s %s %s %s", src_ip, msg.datapath.id, src, dst, msg.in_port)
         # ------------
         self.confidence_award(ev)
         # ------------
@@ -142,7 +165,8 @@ class SimpleSwitch15(app_manager.RyuApp):
 
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
-        in_port = msg.match['in_port']
+        # v 1.5
+        #in_port = msg.match['in_port']
 
         dst = eth.dst
         src = eth.src
@@ -150,33 +174,52 @@ class SimpleSwitch15(app_manager.RyuApp):
         dpid = datapath.id
         self.mac_to_port.setdefault(dpid, {})
 
+        # v 1.5
         # learn a mac address to avoid FLOOD next time.
-        self.mac_to_port[dpid][src] = in_port
+        # self.mac_to_port[dpid][src] = in_port
+
+        # v 1.0
+        # learn a mac address to avoid FLOOD next time.
+        self.mac_to_port[dpid][src] = msg.in_port
+
 
         if dst in self.mac_to_port[dpid]:
             out_port = self.mac_to_port[dpid][dst]
         else:
             out_port = ofproto.OFPP_FLOOD
-            print " Robim flooding"
-            self.logger.info("packet in %s ... %s %s %s %s", src_ip, dpid, src, dst, in_port)
+            #print " Robim flooding"
+            #self.logger.info("packet in %s ... %s %s %s %s", src_ip, dpid, src, dst, in_port)
 
         actions = [parser.OFPActionOutput(out_port)]
 
+        # v 1.5
         # install a flow to avoid packet_in next time
+        # if out_port != ofproto.OFPP_FLOOD:
+        #     match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
+        #     self.add_flow(datapath, 1, match, actions)
+        #     print " Vytvoreny flow"
+        #     self.logger.info("packet in %s ... %s %s %s %s", src_ip, dpid, src, dst, in_port)
+
+        # v 1.0
+            # install a flow to avoid packet_in next time
         if out_port != ofproto.OFPP_FLOOD:
-            match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
-            self.add_flow(datapath, 1, match, actions)
-            print " Vytvoreny flow"
-            self.logger.info("packet in %s ... %s %s %s %s", src_ip, dpid, src, dst, in_port)
+            self.add_flow(datapath, msg.in_port, dst, src, actions)
 
         data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
             data = msg.data
 
-        match = parser.OFPMatch(in_port=in_port)
+        # v 1.5
+        # match = parser.OFPMatch(in_port=in_port)
+        #
+        # out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+        #                           match=match, actions=actions, data=data)
 
-        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
-                                  match=match, actions=actions, data=data)
+        # v 1.0
+        out = datapath.ofproto_parser.OFPPacketOut(
+            datapath=datapath, buffer_id=msg.buffer_id, in_port=msg.in_port,
+            actions=actions, data=data)
+
         datapath.send_msg(out)
 
         # TOBEDELETED Check print
