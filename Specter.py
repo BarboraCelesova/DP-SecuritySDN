6451f3d3e265b26c5a87fb40414d8f2322d8cc39
BarboraCelesova <barbora.celesova@gmail.com>
2019-04-26 21:34:25 +0200

    Final commit

--- Specter.py
+++ Specter.py
@@ -17,7 +17,7 @@ from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
-from ryu.ofproto import ofproto_v1_4
+from ryu.ofproto import ofproto_v1_4, ofproto_v1_3
 from ryu.lib.mac import haddr_to_bin
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
@@ -60,6 +60,7 @@ sem_max_min_confidence_value = threading.Semaphore()
 
 class SimpleSwitch14(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_4.OFP_VERSION]
+    # OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
         #do not delete or comment this print
@@ -78,7 +79,7 @@ class SimpleSwitch14(app_manager.RyuApp):
         CONF.register_opts([
             cfg.IntOpt('NoQueues', default=3),
             cfg.IntOpt('TotalBuffLength', default=10),
-            cfg.IntOpt('Threshold', default=5),
+            cfg.IntOpt('Threshold', default=5)
         ])
 
         number_of_queues = CONF.NoQueues
@@ -98,6 +99,7 @@ class SimpleSwitch14(app_manager.RyuApp):
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
+
         datapath = ev.msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
@@ -113,6 +115,7 @@ class SimpleSwitch14(app_manager.RyuApp):
         actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
                                           ofproto.OFPCML_NO_BUFFER)]
         self.add_flow(datapath, 0, match, actions)
+
 
     def add_flow(self, datapath, priority, match, actions):
         ofproto = datapath.ofproto
